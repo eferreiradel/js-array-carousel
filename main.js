457d514a06e6c2a9526f6a3c680d8b8b168e5616
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-04-19 18:52:26 +0200

    Aggiunte funzionalità bottoni

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 let foto = ["foto1", "foto2", "foto3", "foto4", "foto5"];
 let fotoDentro = foto[0];
 
+let zero = 0;
 document.getElementById("button__next").addEventListener("click", function () {
-  fotoDentro = foto[0 + 1];
+  let add = zero++;
+  fotoDentro = foto[zero];
   document.getElementById("carousel__container").innerHTML = fotoDentro;
+  console.log(fotoDentro);
 });
+
 document.getElementById("button__prev").addEventListener("click", function () {
-  fotoDentro = foto[1 - 1];
+  let subtract = zero--;
+  fotoDentro = foto[zero];
   document.getElementById("carousel__container").innerHTML = fotoDentro;
 });
+
+/*
+document.getElementById("button__next").addEventListener("click", function () {
+
+  fotoDentro = foto[0 + 1] ;
+});
+
+*/
